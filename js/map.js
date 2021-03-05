332efc22d6a1fdf088f3294bbb76fb742465ab5c
taetscher <auteblauwau@hotmail.com>
2021-03-05 18:30:52 +0100

    custom style setup

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map = new mapboxgl.Map({
             container: 'map',
-            style: 'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.leichte-basiskarte.vt/style.json',
+            style: 'mapstyles/style.json',
             hash: false, //set this to true when productive (shows xyz in URL and updates it)
             minzoom: 6,
             maxzoom: 24,
