392e691b7853dc51a9f9f7e973f27ce1f91be380
taetscher <auteblauwau@hotmail.com>
2021-03-06 13:57:54 +0100

    added test ply/line to map (Thun)

---
+++ js/add_ply.js
@@ -0,0 +1,21 @@
+export function addPly(map) {
+    //adds a source and layer to map
+    
+    //in order to use data with mapbox, you need to specify its source
+    map.addSource('ply_test', {
+            type: 'geojson',
+            data: 'geojson/vector_ply_testLyr.geojson'
+        })
+    
+    //only after a source was set can the feature be 
+    map.addLayer({
+        'id': 'molki',
+        'type': 'fill',
+        'source': 'ply_test',
+        'layout': {},
+        'paint': {
+            'fill-color': '#088',
+            'fill-opacity': 0.8
+            }
+        })
+}
--- js/map.js
+++ js/map.js
@@ -1,11 +1,24 @@
+import {addPly} from "../js/add_ply.js";
+import {addLin} from "../js/add_line.js";
+
+//create new map
 var map = new mapboxgl.Map({
             container: 'map',
             style: 'mapstyles/style.json',
             hash: false, //set this to true when productive (shows xyz in URL and updates it)
-            minzoom: 6,
-            maxzoom: 24,
+            minZoom: 6,
+            maxZoom: 16,
             center: [8.38,46.747],
             zoom: 6.2
           });
 
+//add map control (navigation) buttons
 map.addControl(new mapboxgl.NavigationControl());
+
+//on load, add a polygon from geojson source
+map.on('load', function (){
+    
+    addPly(map)
+    addLin(map)
+    
+})
